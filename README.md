467387d85137fef8fac6373d3a8b21ed25bd9a0d
luiztools <luizfduartejr@gmail.com>
2021-01-14 14:53:41 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # nodejs-sendgrid
 A simple Node.js + SendGrid example.
+
+Para o tutorial completo, acesse: https://www.luiztools.com.br/post/como-enviar-emails-em-node-js-usando-sendgrid/
+
+Conheça meus cursos: https://www.luiztools.com.br/meus-cursos
+
+Conheça meus livros: https://www.luiztools.com.br/meus-livros
+
+Me siga nas redes sociais: https://about.me/luiztools
---
+++ index.js
@@ -0,0 +1,21 @@
+(async () => {
+    require('dotenv-safe').config();
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const msg = {
+        to: 'luizfduartejr@gmail.com',
+        from: 'contato@luiztools.com.br',
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+
+    try {
+        const result = await sgMail.send(msg);
+        console.log('Email sent', result);
+    }
+    catch (error) {
+        console.error(error)
+    }
+})();
