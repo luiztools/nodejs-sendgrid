5ecfeaabf93174ff489da9d69ba576e50c72a317
luiztools <luizfduartejr@gmail.com>
2021-01-14 14:54:11 -0300

    minor fix

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
     const msg = {
-        to: 'luizfduartejr@gmail.com',
-        from: 'contato@luiztools.com.br',
+        to: 'email destinatario',
+        from: 'email remetente',
         subject: 'Sending with SendGrid is Fun',
         text: 'and easy to do anywhere, even with Node.js',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
